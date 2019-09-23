b0a775ca8f478752b256821bf9118373fc1df3cb
Mattie Zilaie <your_name@domain.comAccount>
2019-09-23 16:24:04 -0700

    user input favorite animal output user fav animal

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Mahtab Zilaie
+#Date: 9/23/19
+#Description: Asks the user for their favorite animal and then
+#             prints that out that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
